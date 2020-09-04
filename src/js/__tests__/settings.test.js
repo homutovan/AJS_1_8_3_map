11cd79a46d30fa12f4b49ae1eb357c4503dafcb2
Andrey Khomutov <homutovan@live.com>
2020-09-04 17:50:48 +0700

    fix bugs

--- src/js/__tests__/settings.test.js
+++ src/js/__tests__/settings.test.js
@@ -1,81 +1,38 @@
-import Settings from '../settings';
-
-const allSettings = [
-  ['theme', 'dark'],
-  ['theme', 'light'],
-  ['theme', 'gray'],
-  ['music', 'trance'],
-  ['music', 'pop'],
-  ['music', 'rock'],
-  ['music', 'chillout'],
-  ['music', 'off'],
-  ['difficulty', 'easy'],
-  ['difficulty', 'normal'],
-  ['difficulty', 'hard'],
-  ['difficulty', 'nightmare'],
-];
+import Settings, { permissibleSettings as s, defaultSettings as ds } from '../settings';
 
 describe('Check settings', () => {
+  test('Test case №0: checking default settings', () => {
+    const settings = new Settings(ds);
+    expect(settings.settings).toEqual(new Map(ds));
+  });
+
   test('Test case №1: checking default settings', () => {
-    const settings = new Settings();
-    expect(settings.settings).toEqual(new Map([['theme', 'dark'], ['music', 'trance'], ['difficulty', 'easy']]));
+    const settings = new Settings(ds);
+    expect(settings.settings).toEqual(new Map([s[0], s[3], s[8]]));
   });
 
   test('Test case №2: setting user settings', () => {
-    const settings = new Settings();
-    settings.setSettings(...allSettings[1]);
-    expect(settings.settings).toEqual(new Map([allSettings[1], ['music', 'trance'], ['difficulty', 'easy']]));
+    const settings = new Settings(ds);
+    expect(settings.settings).not.toEqual(new Map([s[1], s[3], s[8]]));
   });
 
   test('Test case №3: setting user settings', () => {
-    const settings = new Settings();
-    settings.setSettings(...allSettings[1]);
-    settings.setSettings(...allSettings[2]);
-    expect(settings.settings).toEqual(new Map([allSettings[2], ['music', 'trance'], ['difficulty', 'easy']]));
+    const settings = new Settings([s[1]]);
+    expect(settings.settings).toEqual(new Map([s[1], s[3], s[8]]));
   });
+
   test('Test case №4: setting user settings', () => {
-    const settings = new Settings();
-    settings.setSettings(...allSettings[1]);
-    settings.setSettings(...allSettings[2]);
-    settings.setSettings(...allSettings[4]);
-    expect(settings.settings).toEqual(new Map([allSettings[2], allSettings[4], ['difficulty', 'easy']]));
+    const settings = new Settings([['music', 'blues']]);
+    expect(settings.settings).toEqual(new Map(ds));
   });
+
   test('Test case №5: setting user settings', () => {
-    const settings = new Settings();
-    settings.setSettings(...allSettings[1]);
-    settings.setSettings(...allSettings[2]);
-    settings.setSettings(...allSettings[4]);
-    settings.setSettings(...allSettings[7]);
-    expect(settings.settings).toEqual(new Map([allSettings[2], allSettings[7], ['difficulty', 'easy']]));
+    const settings = new Settings([['music', 'blues'], s[11]]);
+    expect(settings.settings).toEqual(new Map([s[0], s[3], s[11]]));
   });
+
   test('Test case №6: setting user settings', () => {
-    const settings = new Settings();
-    settings.setSettings(...allSettings[1]);
-    settings.setSettings(...allSettings[2]);
-    settings.setSettings(...allSettings[4]);
-    settings.setSettings(...allSettings[7]);
-    settings.setSettings(...allSettings[9]);
-    expect(settings.settings).toEqual(new Map([allSettings[2], allSettings[7], allSettings[9]]));
-  });
-  test('Test case №7: setting user settings', () => {
-    const settings = new Settings();
-    settings.setSettings(...allSettings[1]);
-    settings.setSettings(...allSettings[2]);
-    settings.setSettings(...allSettings[4]);
-    settings.setSettings(...allSettings[7]);
-    settings.setSettings(...allSettings[9]);
-    settings.setSettings(...allSettings[10]);
-    expect(settings.settings).toEqual(new Map([allSettings[2], allSettings[7], allSettings[10]]));
-  });
-  test('Test case №8: setting user settings', () => {
-    const settings = new Settings();
-    settings.setSettings(...allSettings[1]);
-    settings.setSettings(...allSettings[2]);
-    settings.setSettings(...allSettings[4]);
-    settings.setSettings(...allSettings[7]);
-    settings.setSettings(...allSettings[9]);
-    settings.setSettings(...allSettings[10]);
-    settings.setSettings(...allSettings[11]);
-    expect(settings.settings).toEqual(new Map([allSettings[2], allSettings[7], allSettings[11]]));
+    const settings = new Settings([['music', 'blues'], s[11]]);
+    expect(settings.settings).not.toEqual(new Map([s[1], s[3], s[11]]));
   });
 });
--- src/js/settings.js
+++ src/js/settings.js
@@ -1,17 +1,36 @@
-const defaultSettings = [
-  { theme: 'dark' },
-  { music: 'trance' },
-  { difficulty: 'easy' },
+export const defaultSettings = [
+  ['theme', 'dark'],
+  ['music', 'trance'],
+  ['difficulty', 'easy'],
+];
+
+export const permissibleSettings = [
+  ['theme', 'dark'],
+  ['theme', 'light'],
+  ['theme', 'gray'],
+  ['music', 'trance'],
+  ['music', 'pop'],
+  ['music', 'rock'],
+  ['music', 'chillout'],
+  ['music', 'off'],
+  ['difficulty', 'easy'],
+  ['difficulty', 'normal'],
+  ['difficulty', 'hard'],
+  ['difficulty', 'nightmare'],
 ];
 
 export default class Settings {
-  constructor() {
-    this.defaultSettings = new Map(defaultSettings.map((setting) => Object.entries(setting)[0]));
-    this.userSettings = new Map();
+  constructor(userSettings) {
+    this.defaultSettings = new Map(defaultSettings);
+    this.permissibleSettings = permissibleSettings;
+    this.userSettings = new Map(this.filterSettings(userSettings));
   }
 
-  setSettings(key, value) {
-    this.userSettings.set(key, value);
+  filterSettings(settings) {
+    return settings
+      .filter((setting) => this.permissibleSettings
+        .some((element) => setting
+          .every((e, i) => e === element[i])));
   }
 
   get settings() {
